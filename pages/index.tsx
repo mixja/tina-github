0dd955e80fb56719fa40cbf36057e7da651178ca
Justin Menga <justin.menga@gmail.com>
2020-06-11 13:37:32 +1200

    Load content from GitHub

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+export default function Home({ file }) {
+  const data = file.data
+
   return (
     <div className="container">
       <Head>
@@ -10,7 +14,8 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
+          {data.title}
         </h1>
 
         <p className="description">
@@ -206,4 +211,26 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async function({preview, previewData}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: 'content/home.json',
+      parse: parseJson,
+    })
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/home.json',
+        data: (await import('../content/home.json')).default,
+      },
+    },
+  }
 }
