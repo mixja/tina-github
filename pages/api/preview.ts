a069b8051b47deb5dd2ec22b0af4f2e4c898b07b
Justin Menga <justin.menga@gmail.com>
2020-06-13 21:44:45 +1200

    Use github personal access token for authentication

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,3 +1,20 @@
 import { previewHandler } from 'next-tinacms-github'
 
-export default previewHandler
+export const ACCESS_TOKEN_KEY = 'github_access_token'
+export const WORKING_REPO_KEY = 'working_repo_full_name'
+export const HEAD_BRANCH_KEY = 'head_branch'
+
+export const authenticatedPreviewHandler = (req: any, res: any) => {
+  const previewData = {
+    working_repo_full_name: req.cookies[WORKING_REPO_KEY],
+    github_access_token: process.env.GITHUB_TOKEN,
+    head_branch:
+      req.cookies[HEAD_BRANCH_KEY] || process.env.BASE_BRANCH || 'master',
+  }
+  res.setPreviewData(previewData)
+  res.status(200).end()
+}
+
+// export default previewHandler
+export default authenticatedPreviewHandler
+
--- pages/api/proxy-github.ts
+++ pages/api/proxy-github.ts
@@ -1,3 +1,27 @@
 import { apiProxy } from 'next-tinacms-github'
 
-export default apiProxy
+const axios = require('axios')
+
+export const authenticatedApiProxy = (req: any, res: any) => {
+  const { headers, ...data } = JSON.parse(req.body)
+  console.log("request")
+
+  axios({
+    ...data,
+    headers: {
+      ...headers,
+      Authorization: 'token ' + process.env.GITHUB_TOKEN,
+    },
+  })
+    .then((resp: any) => {
+      console.log("response")
+      res.status(resp.status).json(resp.data)
+    })
+    .catch((err: any) => {
+      console.log("error")
+      res.status(err.response.status).json(err.response.data)
+    })
+}
+
+// export default apiProxy
+export default authenticatedApiProxy
